2772749b95f7c49cb29ff981e4e29c13db29e6e4
Emily Jirles <emily.jirles@gmail.com>
2018-07-29 00:07:54 +0000

    automatically backed up by learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -3,7 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default class Keypad extends React.Component {
-  let printMessage = () => {
+  printMessage = () => {
     console.log('Entering password...');
     return;
   }
